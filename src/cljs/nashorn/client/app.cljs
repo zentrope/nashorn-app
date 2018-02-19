94018d493301c037c6852aa891c43217e6a02647
Keith Irwin <keith@zentrope.com>
2018-02-18 22:31:35 -0800

    Add a "new script" button to the sidebar

--- src/cljs/nashorn/client/app.cljs
+++ src/cljs/nashorn/client/app.cljs
@@ -1,26 +1,38 @@
 (ns nashorn.client.app
   (:require
    [cljs.core.async :refer [put!]]
+   [clojure.walk :refer [postwalk]]
    [rum.core :as rum :refer [defc]]))
 
 ;; component helpers
 
-(defn send!
+(defn- send!
   [ch event & [msg]]
   (fn [e]
     (let [msg (if (nil? msg) {} msg)]
       (put! ch (merge {:event event} msg)))))
 
-(def PureMixin rum/static)
+(defn- clean
+  [data]
+  (postwalk #(if (fn? %) :clean/fn %) data))
+
+(def ^:private PureMixin
+  {:should-update (fn [old new]
+                    (not= (-> old :rum/args clean)
+                          (-> new :rum/args clean)))})
+
+(defc Button < PureMixin
+  [{:keys [onClick label]}]
+  [:div.Button {:onClick onClick} label])
 
 (defn- script-item
-  [name]
-  [:div.Item [:span.Script "= "] name])
+  [name ch]
+  [:div.Item {:key name} [:span.Script "= "] name])
 
 (defn- fn-item
   [{:keys [name] :as function-doc} ch]
   [:div.Item {:key name
-              :onClick (send! ch :function/show function-doc)}
+              :onClick (send! ch :function/show {:function function-doc})}
    [:span.Fn "\u03bb "] name])
 
 (def ^:private sb-footer
@@ -28,6 +40,14 @@
 
 (def ^:private sb-header
   [:div.Header [:div.Title "Extension Editor"]])
+
+(defc SideBarScriptsPanel < PureMixin
+  [scripts ch]
+  [:section.Panel
+   [:div.Title "Scripts"]
+   [:div.Body
+    (for [script (sort scripts)]
+      (script-item script ch))]])
 
 (defc SideBarFunctionsPanel < PureMixin
   [functions ch]
@@ -42,13 +62,11 @@
   [:section.SideBar
    sb-header
    [:div.Panels
-    [:section.Panel
-     [:div.Title "Scripts"]
-     [:div.Body
-      (script-item "github.js")]]
+    (SideBarScriptsPanel ["github.js"] ch)
     (SideBarFunctionsPanel (:functions state) ch)]
-   sb-footer
-   ])
+   [:div.Buttons
+    (Button {:onClick (send! ch :script/new) :label "New Script"})]
+   sb-footer])
 
 (defc WorkArea < PureMixin
   [state ch]
