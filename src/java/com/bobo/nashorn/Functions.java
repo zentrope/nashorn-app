1dc19bdd486a8daa9e27a03563a050842346f982
Keith Irwin <keith@zentrope.com>
2018-03-11 17:05:52 -0700

    Reimplement script http lib to support redirects

--- src/java/com/bobo/nashorn/Functions.java
+++ src/java/com/bobo/nashorn/Functions.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -53,16 +54,49 @@ public final class Functions {
     return new JSONObject(json);
   }
 
-  public static Map<String,Object> httpGet(String urlString) throws Exception {
+  public static Map<String,Object> httpGet(String urlString) throws IOException {
     return httpGet(urlString, null);
   }
 
-  public static Map<String,Object> httpGet(String urlString, Map<String, Object> headers) throws Exception {
-    try {
-      URL url = new URL(urlString);
-      HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-      conn.setRequestMethod("GET");
-      conn.setRequestProperty("User-Agent", "bobo/nashorn");
+  public static Map<String,Object> httpGet(String urlString, Map<String, Object> headers) throws IOException {
+    HttpURLConnection conn = doGET(urlString, headers);
+    Map<String, Object> response = makeResponse(conn);
+    conn.disconnect();
+    return response;
+  }
+
+  public static Map<String, Object> httpPost(String urlString, Map<String, Object> headers, String data) throws IOException {
+    HttpURLConnection conn = doPOST(urlString, headers, data);
+    Map<String, Object> response = makeResponse(conn);
+    conn.disconnect();
+    return response;
+  }
+
+  //---------------------------------------------------------------------------
+
+  private static HttpURLConnection doGET(String urlString, Map<String, Object> headers) throws IOException {
+    return doConnect("GET", urlString, headers, null);
+  }
+
+  private static HttpURLConnection doPOST(String urlString, Map<String, Object> headers, String data) throws IOException {
+    return doConnect("POST", urlString, headers, data);
+  }
+
+  private static HttpURLConnection doConnect(String method, String urlString, Map<String, Object> headers, String data) throws IOException {
+    URL resourceUrl, base, next;
+    HttpURLConnection conn;
+    String url = urlString;
+    String location;
+
+    int maxTries = 10;
+    int tries = 0;
+
+    while (true) {
+      resourceUrl = new URL(url);
+      conn = (HttpURLConnection)resourceUrl.openConnection();
+      conn.setInstanceFollowRedirects(false);
+      conn.setRequestMethod(method);
+      conn.setRequestProperty("User-Agent", "bobo");
 
       if (headers != null) {
         for (String header : headers.keySet()) {
@@ -71,48 +105,32 @@ public final class Functions {
         }
       }
 
-      conn.connect();
-
-      Map<String, Object> response = makeResponse(conn);
-
-      conn.disconnect();
-      return response;
-    }
-    catch (Throwable t) {
-      System.out.printf("ERROR: %s\n", t);
-      throw t;
-    }
-  }
-
-  public static Map<String, Object> httpPost(String urlString, Map<String, Object> headers, String data) throws Exception {
-    try {
-      URL url = new URL(urlString);
-      HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-
-      conn.setRequestMethod("POST");
-      conn.setRequestProperty("User-Agent", "bobo/nashorn");
-
-      for (String header : headers.keySet()) {
-        String value = (String)headers.get(header);
-        conn.setRequestProperty(header, value);
+      if (method == "POST" && data != null) {
+        conn.setDoOutput(true);
+        DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+        out.writeBytes(data);
+        out.flush();
+        out.close();
       }
 
-      conn.setDoOutput(true);
+      tries += 1;
+      switch (conn.getResponseCode()) {
+        case HttpURLConnection.HTTP_MOVED_PERM:
+        case HttpURLConnection.HTTP_MOVED_TEMP:
+          if (tries >= maxTries) {
+            throw new IOException(String.format("Redirect exceeded %s attempts.", maxTries));
+          }
 
-      DataOutputStream out = new DataOutputStream(conn.getOutputStream());
-      out.writeBytes(data);
-      out.flush();
-      out.close();
-
-      Map<String, Object> response = makeResponse(conn);
-
-      conn.disconnect();
-      return response;
+          location = conn.getHeaderField("Location");
+          location = URLDecoder.decode(location, "UTF-8");
+          base = new URL(url);
+          next = new URL(base, location);
+          url = next.toExternalForm();
+          continue;
+      }
+      break;
     }
-    catch (Throwable t) {
-      System.out.printf("ERROR: %s\n", t);
-      throw t;
-    }
+    return conn;
   }
 
   private static Map<String, Object> makeResponse(HttpURLConnection conn) throws IOException {
